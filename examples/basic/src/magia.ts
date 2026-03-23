17e6b36ac10efb85ae117d963a9529d908653c2b
Athena Freitas <athenacfr@gmail.com>
2026-03-22 23:36:56 -0400

    fix: move manifest into createMagia config object in basic example

--- examples/basic/src/magia.ts
+++ examples/basic/src/magia.ts
@@ -1,10 +1,11 @@
-import { createMagia } from 'magia-api'
-import { manifest } from './magia.gen'
+import { createMagia } from "magia-api";
+import { manifest } from "./magia.gen";
 
 export const magia = createMagia({
+  manifest,
   apis: {
     petstore: {
-      baseUrl: 'https://petstore3.swagger.io/api/v3',
+      baseUrl: "https://petstore3.swagger.io/api/v3",
     },
   },
-}, manifest)
+});
